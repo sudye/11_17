ecf9db14f171cf864d3f7681772b3899ddae02e3
sudye <simjaye@naver.com>
2022-11-17 09:35:26 +0900

    xx

---
+++ src/main/java/com/example/demo/Demotest32Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demotest32Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demotest32Application.class, args);
+	}
+
+}
